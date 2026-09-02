a192d45102d7bdce0c05f5c0810f9a61387166d8
Susmitha Chowdary <susmithachowdary14@gmail.com>
2026-09-02 14:17:56 -0400

    Prepare application for deployment

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
 def add(a, b):
     return a + b
 
@@ -9,8 +14,24 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+
 def divide(a, b):
     return a / b
 
+
 def square(a):
     return a * a
+
+
+@app.route("/")
+def home():
+    return "Week 7 CI/CD deployment is working!"
+
+
+@app.route("/health")
+def health():
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
